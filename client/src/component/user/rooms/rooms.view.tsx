2fd8c26792fbe8bdcbd4b7cea5cb19384cb869f6
dtrucinuet <22028179@vnu.edu.vn>
2024-12-17 11:42:25 +0700

    feat(client): create rooms and rating component

---
+++ client/src/component/user/rooms/rooms.view.tsx
@@ -0,0 +1,86 @@
+'use client';
+import { Button, Group, Container, Title, Text, Pagination, Input } from '@mantine/core';
+import { useState } from 'react';
+import styles from './rooms.module.css';
+import RoomRating from '@/component/rating/rating';
+
+const Rooms = (props: any) => {
+    const itemsPerPage = 6;
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const filteredData = props.data.filter((room: any) =>
+        room.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+
+    const paginateData = () => {
+        const startIndex = (currentPage - 1) * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage;
+        return filteredData.slice(startIndex, endIndex);
+    };
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+    };
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+        setCurrentPage(1);
+    };
+
+    const handleDetailRoom = (roomId: string) => {
+        console.log(`Update permissions for: ${roomId}`);
+    };
+
+    return (
+        <Container size="lg">
+            <div className={styles.titleContainer}>
+                <Title className={styles.title}>Các phòng tập</Title>
+            </div>
+
+            <div className={styles.searchContainer}>
+                <Input
+                    id="box"
+                    placeholder="Search anything..."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    className={styles.search__box}
+                />
+                <i className={styles.search__icon} id="icon"></i>
+            </div>
+
+            <div className={styles.cardsContainer}>
+                {paginateData().map((room: any) => (
+                    <div key={room.id} className={styles.card}>
+                        <img src="/phongtap2.png" alt={room.name} className={styles.cardImage} />
+                        <div className={styles.cardContent}>
+                            <Title order={4}>{room.name}</Title>
+                            <Text>{room.description}</Text>
+                            <RoomRating rate={room} />
+                            <Group>
+                                <Button color="blue" onClick={() => handleDetailRoom(room.id)}>
+                                    Chi tiết
+                                </Button>
+                            </Group>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <Group justify="center" mt="md">
+                <Pagination
+                    className={styles.paginationGroup}
+                    value={currentPage}
+                    onChange={handlePageChange}
+                    total={totalPages}
+                    withEdges
+                />
+            </Group>
+        </Container>
+    );
+};
+
+export default Rooms;
