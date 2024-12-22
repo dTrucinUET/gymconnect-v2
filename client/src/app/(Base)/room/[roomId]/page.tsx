e78a2ed83ff4c7e124d3bf5d7d01edf6d3c4a68f
Lee-412 <leeduchht@gmail.com>
2024-12-23 02:50:22 +0700

    feat(client/service): done service managerment, fetch main

--- client/src/app/(Base)/room/[roomId]/page.tsx
+++ client/src/app/(Base)/room/[roomId]/page.tsx
@@ -1,16 +1,16 @@
 import { notFound } from 'next/navigation';
-import RoomDetail from "@/component/user/room/room"; 
+import RoomDetail from "@/component/user/room/room";
 // import Comment from "@/component/comment/comment"; 
 
 
 interface RoomPageProps {
-    params: { roomId: string }; 
+    params: { roomId: string };
 }
 
 const RoomPage = async ({ params }: RoomPageProps) => {
-    const { roomId } = await Promise.resolve(params); 
+    const { roomId } = await Promise.resolve(params);
     console.log(roomId);
-    
+
     const response = await fetch(`http://localhost:8080/room/${roomId}`);
     const room = await response.json();
 
@@ -20,14 +20,14 @@ const RoomPage = async ({ params }: RoomPageProps) => {
 
 
     const responseComment = await fetch(`http://localhost:8080/room_comment/room/${roomId}`);
-    const comments = await responseComment.json(); 
-    console.log("comment", typeof(comments));
-    
+    const comments = await responseComment.json();
+    console.log("comment", typeof (comments));
+
 
     return (
         <>
             <RoomDetail room={room} />
-            <Comment comments={comments} />
+            {/* <Comment comments={comments} /> */}
         </>
     );
 };
